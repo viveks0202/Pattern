5c80e63395159cd05bbb55d30f7cf29bf8e69f83
viveks0202 <vivek.pundir33@gmail.com>
2023-03-20 08:17:55 +0530

    First commit

---
+++ eLetter.java
@@ -0,0 +1,20 @@
+// package Pattern;
+import java.util.*;
+
+public class eLetter {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+    if(i==0||i==(n-1)/2||i==n-1||j==0){
+        System.out.print("*");
+    }
+    else{
+        System.out.print(" ");
+    }
+}
+System.out.println();
+}
+    }
+}
